ae25f4d7a7b9ce2a493f1a4faa9a3dec2c5fbd1a
John Cabuguason <jcalc30@gmail.com>
2020-04-28 15:52:09 -0400

    Version 1.4 -New GUI design introduced (nightmode)
    Future Tasks:
    - integrate math operator functions in Calculator.java into the 
    controller class

--- src/computerengineeringcalculator/CalculatorController.java
+++ src/computerengineeringcalculator/CalculatorController.java
@@ -6,13 +6,12 @@
 package computerengineeringcalculator; 
  
 import java.net.URL; 
+import java.util.ArrayList;
 import java.util.ResourceBundle; 
 import javafx.event.ActionEvent; 
 import javafx.fxml.FXML; 
 import javafx.fxml.Initializable; 
 import javafx.scene.control.Label; 
-import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Rectangle; 
  
 /** 
  * FXML Controller class 
@@ -24,10 +23,12 @@ public class CalculatorController implements Initializable
  
       
  
-       String storedOutput = "";
-       private double previousNumber = 0.0; 
+       // string with delimiters
+       private String currentOutput = "";
+       
+       //implementation of previous answer memory 
        private double previousAnswer = 0.0; 
-       private boolean equalsPressed; 
+       
        @FXML 
        private Label outputLabel; 
        @FXML
@@ -43,12 +44,12 @@ public class CalculatorController implements Initializable
  
        @FXML 
        private void decToHex(ActionEvent event) { 
-              answerLabel.setText(Calculator.decToHex(Integer.parseInt(outputLabel.getText())));
+              answerLabel.setText(Calculator.decToHex(Integer.parseInt(currentOutput)));
        } 
  
        @FXML 
        private void decToBin(ActionEvent event) { 
-              answerLabel.setText(""+Calculator.decToBin(Integer.parseInt(outputLabel.getText())));
+              answerLabel.setText(Calculator.decToBin(Integer.parseInt(outputLabel.getText())));
        } 
  
        @FXML 
@@ -58,130 +59,146 @@ public class CalculatorController implements Initializable
  
        @FXML 
        private void hexToDec(ActionEvent event) { 
-              answerLabel.setText(""+Calculator.hexToDec(outputLabel.getText()));
+              answerLabel.setText( Calculator.hexToDec(outputLabel.getText()) + "");
        } 
  
        @FXML 
        private void hexToBin(ActionEvent event) { 
-              answerLabel.setText(""+Calculator.hexToBin(outputLabel.getText()));
+              answerLabel.setText( ""+ Calculator.hexToBin(outputLabel.getText()));
        } 
  
        @FXML 
-       private void binToHex(ActionEvent event) { 
-              answerLabel.setText(""+Calculator.binToHex(Integer.parseInt(outputLabel.getText())));
+       private void binToHex(ActionEvent event) {
+              answerLabel.setText(Calculator.binToHex(Integer.parseInt(outputLabel.getText())));
        } 
         
         
-       // Number Input Buttons 
+/********************************************Number Input Buttons******************************************************************/
        @FXML 
        private void numZero(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "0"; 
-              outputLabel.setText(currentOutput);       
+              currentOutput += "0";
+              outputLabel.setText(outputLabel.getText() + "0");
        } 
  
        @FXML 
        private void numOne(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "1"; 
-              outputLabel.setText(currentOutput); 
+              currentOutput += "0";
+              outputLabel.setText(outputLabel.getText() + "1");
        } 
  
        @FXML 
-       private void numTwo(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "2"; 
-              outputLabel.setText(currentOutput); 
+       private void numTwo(ActionEvent event) {
+              currentOutput += "2";
+              outputLabel.setText(outputLabel.getText() + "2");
        }       
  
        @FXML 
        private void numThree(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "3"; 
-              outputLabel.setText(currentOutput); 
+              currentOutput += "3";
+              outputLabel.setText(outputLabel.getText() + "3");
        } 
         
        @FXML 
        private void numFour(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "4"; 
-              outputLabel.setText(currentOutput); 
+              currentOutput += "4";
+              outputLabel.setText(outputLabel.getText() + "4");
        } 
         
        @FXML 
        private void numFive(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "5"; 
-              outputLabel.setText(currentOutput); 
+              currentOutput += "5";
+              outputLabel.setText(outputLabel.getText() + "5");
        } 
         
        @FXML 
        private void numSix(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "6"; 
-              outputLabel.setText(currentOutput); 
+              currentOutput += "6";
+              outputLabel.setText(outputLabel.getText() + "6");
        } 
         
        @FXML 
-       private void numSeven(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "7"; 
-              outputLabel.setText(currentOutput); 
+       private void numSeven(ActionEvent event) {
+              currentOutput += "7";
+              outputLabel.setText(outputLabel.getText() + "7");
        } 
  
        @FXML 
-       private void numEight(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "8"; 
-              outputLabel.setText(currentOutput); 
+       private void numEight(ActionEvent event) {
+              currentOutput += "8";
+              outputLabel.setText(outputLabel.getText() + "8");
        } 
  
        @FXML 
-       private void numNine(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "9"; 
-              outputLabel.setText(currentOutput); 
+       private void numNine(ActionEvent event) {
+              currentOutput += "9";
+              outputLabel.setText(outputLabel.getText() + "9");
        } 
         
  
-       //  Buttons for Operators 
+/*****************************************Math Operator Buttons.******************************************************************/
         
        @FXML 
        private void dotButton(ActionEvent event) { 
-              String currentOutput = outputLabel.getText()+ "."; 
-              outputLabel.setText(currentOutput); 
+              currentOutput = outputLabel.getText()+ "."; 
+              outputLabel.setText(outputLabel.getText() + "."); 
        } 
  
        @FXML 
        private void expButton(ActionEvent event) { 
        } 
-        
+       
        @FXML 
-       private void multiplyButton(ActionEvent event) {                 
-              outputLabel.setText( outputLabel.getText() + ",*,"); 
+       private void multiplyButton(ActionEvent event) {    
+              currentOutput += ",*,";
+              outputLabel.setText( outputLabel.getText() + "*"); 
        } 
  
        @FXML 
        private void divideButton(ActionEvent event) { 
-                 outputLabel.setText( outputLabel.getText() + ",/,"); 
+              currentOutput += ",/,";
+              outputLabel.setText( outputLabel.getText() + "/"); 
        } 
  
        @FXML 
        private void addButton(ActionEvent event) { 
-                outputLabel.setText( outputLabel.getText() + ",+,"); 
-               
+                currentOutput += ",+,";
+                outputLabel.setText( outputLabel.getText() + "+"); 
        } 
  
        @FXML 
        private void subtractButton(ActionEvent event) { 
+                currentOutput += ",-,";
                 outputLabel.setText( outputLabel.getText() + "-"); 
-         
        } 
         
+       
        @FXML 
        private void acButton(ActionEvent event) { 
               outputLabel.setText(""); 
               answerLabel.setText("");
+              currentOutput  = "";          
        } 
  
        @FXML 
        private void deleteButton(ActionEvent event) { 
-              String currentOutput = outputLabel.getText(); 
-              if(currentOutput.length() > 0)
-              outputLabel.setText(currentOutput.substring(0,currentOutput.length()-1)); 
+         
+              //deletes one character from output label
+              if(!outputLabel.getText().isEmpty())       
+                     outputLabel.setText(outputLabel.getText().substring(0,outputLabel.getText().length()-1));  
+              
+              //coincides with output label
+              if(!currentOutput.isEmpty()){
+                     // if operator , deletes operator substring 
+                     if(currentOutput.charAt(currentOutput.length() -1) == ',' )
+                            currentOutput = currentOutput.substring(0,currentOutput.length()-3);
+                     // else deletes one character 
+                     else
+                            currentOutput = currentOutput.substring(0,currentOutput.length() -1);    
+              }
        } 
+       
         
-       //not implemented yet
+       //NOT IMPLEMENTED YET
         @FXML 
        private void prevAnsButton(ActionEvent event) { 
               outputLabel.setText(outputLabel.getText()+",Ans,"); 
@@ -189,30 +206,41 @@ public class CalculatorController implements Initializable
  
        @FXML 
        private void equalButton(ActionEvent event) { 
-              // implement stack and queue operation on operator functions  
-              String [] operate =  outputLabel.getText().split(",");
-               double lastNum = 0;
-              for(int i = 0; i<operate.length;i++){
-                     
-                     lastNum = Double.parseDouble(operate[i]);
-                     
-                     switch(operate[++i]){
+              
+              ArrayList<String> chList = new ArrayList<String>();
+              String [] chArr =  currentOutput.split(",");   
+              //convert to char String ArrayList 
+              for(int i = 0; i < chArr.length;i++){
+                     chList.add(chArr[i]);
+              }
+              boolean isOperator = false;
+              // gets next operator
+              //calculates output of substring of left num + operator + right num 
+              //simplifies original string with calculated output
+              for(int  i = 0; i < chList.size(); i++){
+                     switch(chList.get(i)){
                             case "+":
-                                   lastNum += Double.parseDouble(operate[++i]);
+                                   previousAnswer = Double.parseDouble(chList.get(i-1)) + Double.parseDouble(chList.get(i+1));
+                                   isOperator = true;
                                    break;
-                            case "-":
-                                   lastNum -= Double.parseDouble(operate[++i]);
-                                    break;
                             case "*":
-                                   lastNum *= Double.parseDouble(operate[++i]);
+                                   previousAnswer = Double.parseDouble(chList.get(i-1)) * Double.parseDouble(chList.get(i+1));
+                                   isOperator = true;
                                    break;
                             case "/":
-                                   lastNum /= Double.parseDouble(operate[++i]);
+                                   previousAnswer = Double.parseDouble(chList.get(i-1)) / Double.parseDouble(chList.get(i+1));
+                                   isOperator = true;
+                                   break;
                      }
-              }
-              
-              answerLabel.setText(lastNum+"");
-              
+                     if(isOperator){ 
+                            chList.set(i+1, previousAnswer+"");
+                            chList.remove(i);
+                            chList.remove(i-1);              
+                            i --;
+                            isOperator = false;
+                     }  
+             }       
+              answerLabel.setText(previousAnswer+"");       
        } 
 
        
@@ -220,30 +248,36 @@ public class CalculatorController implements Initializable
        @FXML
        private void buttonA(ActionEvent event) {
               outputLabel.setText(outputLabel.getText()+ "A"); 
+              currentOutput += "A";
        }
        
        @FXML
        private void buttonB(ActionEvent event) {
                outputLabel.setText(outputLabel.getText()+ "B"); 
+               currentOutput += "B";
        }
        @FXML
        private void buttonC(ActionEvent event) {
                outputLabel.setText(outputLabel.getText()+ "C"); 
+               currentOutput += "C";
        }
        
        @FXML
        private void buttonD(ActionEvent event) {
                outputLabel.setText(outputLabel.getText()+ "D"); 
+               currentOutput += "D";
        }
        
        @FXML
        private void buttonE(ActionEvent event) {
                outputLabel.setText(outputLabel.getText()+ "E"); 
+               currentOutput += "E";
        }
 
        @FXML
        private void buttonF(ActionEvent event) {
                outputLabel.setText(outputLabel.getText()+ "F"); 
+               currentOutput += "F";
        }
 
 
